fec0c33991fd24b38fcec61edcb4b6c9132c79bf
Ryan Hecht <ryan.hecht123@gmail.com>
2025-04-01 23:23:58 -0400

    Add better features

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 """Cryptocurrency analysis and prediction package."""
 
 from .data import CoinGeckoAPIScraper, StablecoinFilter
+from .features import CryptoFeatureEngineer, add_crypto_features
 
-__all__ = ['CoinGeckoAPIScraper', 'StablecoinFilter']
+__all__ = ['CoinGeckoAPIScraper', 'StablecoinFilter', 'CryptoFeatureEngineer', 'add_crypto_features']
 
 __version__ = '0.1.0'
--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,1 +1,6 @@
 """Feature engineering modules."""
+
+# Import the main feature engineering module
+from .crypto_feature_engineering import CryptoFeatureEngineer, add_crypto_features
+
+__all__ = ['CryptoFeatureEngineer', 'add_crypto_features']
